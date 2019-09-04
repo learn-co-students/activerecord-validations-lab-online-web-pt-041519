6fb19f6f3c279f151f61c09d03a05b202273d60d
aparkening <aaron.parkening@gmail.com>
2019-09-04 17:16:57 -0400

    Done. Add validation to author and post models.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
+class TitleValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /Won't Believe|Secret|Top \d*|Guess/
+      record.errors[attribute] << (options[:message] || "is not a title")
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true, title: true
+  validates :content, length: { minimum: 250 } 
+  validates :summary, length: { maximum: 250 } 
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid category" }
+
 end
