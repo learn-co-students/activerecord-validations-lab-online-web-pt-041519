29f4480dae74bde05eca4cbd5bd4b3dedb3c98ac
earthctzn <earthctzn1@gmail.com>
2019-09-05 01:37:21 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,12 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {within: %w(Fiction Non-Fiction)}
-  
-  
+  validate :clickbaity
+end
+
+def clickbaity
+  bait = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+  if bait.none? { |post| post.match title }
+    errors.add(:title, "not enough baityness")
+  end
 end
