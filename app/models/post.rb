5d79cf2bea4f081f5b4dfce7535ce16b0d2a2a31
kjablonski08 <kjablonski08@gmail.com>
2019-09-08 13:45:51 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true 
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid option" } 
+  validate :clickbaity
+    def clickbaity
+      if title && title.exclude?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+        errors.add(:title, "Not Clickbait-y enough")
+      end
+    end
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
 end
